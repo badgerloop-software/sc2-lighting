04778cd0797d7a7e4b180242c8d5eae1b54d087c
Will Wentink <wwentink@wisc.edu>
2026-08-07 01:51:18 -0500

    cleanup: cleaned up comments and organization a bit, pruned dead code, also updated bps light id

--- src/lightingCAN.cpp
+++ src/lightingCAN.cpp
@@ -1,3 +1,5 @@
+// lightingCAN: map CAN bits to two local LED channels (PA0, PA1)
+// BOARD macro in the header selects which frames and bits this build uses
 #include "lightingCAN.h"
 
 volatile LED leds[2] = {
@@ -5,18 +7,33 @@ volatile LED leds[2] = {
     {false, false, BLINK2, PA1}
 };
 
-void setLED(uint8_t i);
+static void setLED(uint8_t i) {
+    volatile LED* led = &leds[i];
+    if (led->on) {
+        if (led->blinks) {
+            // Determines blink phase
+            led->blink_val = ((millis() / BLINK_DELAY) % 2) != 0;
+            digitalWrite(led->pin, led->blink_val);
+        } else {
+            digitalWrite(led->pin, HIGH);
+        }
+    } else {
+        digitalWrite(led->pin, LOW);
+    }
+}
 
 LightingCAN::LightingCAN(CAN_TypeDef* canPort, CAN_PINS pins, int frequency) : CANManager(canPort, pins, frequency) {
 
 };
 
+// Static send message for testing
 bool LightingCAN::send() {
     uint8_t abc = 12;
     return this->sendMessage(0x700, (void*)&abc, sizeof(uint8_t));
 }
 
 void LightingCAN::updateOutputs() {
+    // Calls setLED for each LED that is on and blinking (note: bps only, others listen to steering wheel)
     for (uint8_t i = 0; i < 2; i++) {
         if (leds[i].on && leds[i].blinks) {
             setLED(i);
@@ -28,6 +45,7 @@ void LightingCAN::readHandler(CAN_message_t msg) {
     uint8_t data = msg.buf[0];
 
 #if defined(LEFT_BLINK) || defined(RIGHT_BLINK)
+    // 0x300 from steering wheel: blink bits in the digital pack to synchronize
     if (msg.id == LED_ID_1) {
         leds[0].on = (data >> BIT_OFF1) & 1;
         setLED(0);
@@ -42,6 +60,7 @@ void LightingCAN::readHandler(CAN_message_t msg) {
 #endif
 
 #if defined(BPS_FAULT)
+    // 0x001 from powertrain: fault bit drives the BPS lamp
     if (msg.id == LED_ID_2) {
         leds[1].on = (data >> BIT_OFF2) & 1;
         setLED(1);
@@ -54,18 +73,4 @@ void LightingCAN::readHandler(CAN_message_t msg) {
         setLED(1);
     }
 #endif
-}
-
-void setLED(uint8_t i) {
-    volatile LED* led = &leds[i];
-    if (led->on) {
-        if (led->blinks) {
-            led->blink_val = ((millis() / BLINK_DELAY) % 2) != 0;
-            digitalWrite(led->pin, led->blink_val);
-        } else {
-            digitalWrite(led->pin, HIGH);
-        }
-    } else {
-        digitalWrite(led->pin, LOW);
-    }
 }
